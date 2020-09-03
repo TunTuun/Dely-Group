d8f5e9c107573fc96353f5a8ae19af3439da8232
TunTuun <ikedzawa@mail.ru>
2020-09-03 23:23:48 +0300

    2

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Header dropdown
+function headerdropdown() {
+    document.querySelector(".header-dropdown").classList.toggle("mobile-toggle");
+}
+header_dropdown.onclick = headerdropdown;
+header_dropdown_close_button.onclick = headerdropdown;
