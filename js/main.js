0a976582a050595fcdb149b17ee0d28b0d6f0534
TunTuun <ikedzawa@mail.ru>
2020-09-07 22:14:15 +0300

    4

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ function headerdropdown() {
 function account_section_show() {
     document.querySelector(".authorization-showup").classList.toggle("mobile-toggle");
 }
+function account_section_registration_show() {
+    document.querySelector(".authorization-showup").classList.toggle("mobile-toggle");
+    document.querySelector(".registration-showup").classList.toggle("mobile-toggle");
+}
+function account_section_registration_close() {
+    document.querySelector(".registration-showup").classList.toggle("mobile-toggle");
+}
 header_dropdown.onclick = headerdropdown;
 header_dropdown_close_button.onclick = headerdropdown;
 account_button.onclick = account_section_show;
