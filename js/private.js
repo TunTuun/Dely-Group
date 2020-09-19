c8e28396070f539fe6f14401762b197dbfce5a91
TunTuun <ikedzawa@mail.ru>
2020-09-19 23:21:01 +0300

    .

--- js/private.js
+++ js/private.js
@@ -1,8 +1,10 @@
 "use strict";
+// Menu accordion
 let accItem = document.getElementsByClassName('side-menu__element');
 let accHD = document.getElementsByClassName('side-menu__heading');
 let accArrows = document.getElementsByClassName('side-menu__arrow');
 let i;
+let k;
 for (i = 0; i < accHD.length; i++) {
     accHD[i].addEventListener('click', toggleItem, false);
 }
@@ -22,3 +24,34 @@ function toggleItem() {
         }
     }
 }
+// Pick up selection
+let rateElements = document.getElementsByClassName('rate-pick__element');
+// Обработка клика на выбор тарифного плана
+function pickRate() {
+    let pickItem = this.className;
+    for (i = 0; i < rateElements.length; i++) {
+        if (rateElements[i].classList.contains('rate-pick__element_active')) {
+            rateElements[i].classList.toggle('rate-pick__element_active');
+        }
+        let mas = rateElements[i].getElementsByClassName('rate-pick__description');
+        for (k = 0; k < mas.length; k++) {
+            if (mas[k].classList.contains('rate-pick__description_active')) {
+                mas[k].classList.toggle('rate-pick__description_active');
+            }
+        }
+    }
+    this.classList.toggle("rate-pick__element_active");
+    let mas = this.getElementsByClassName('rate-pick__description');
+    for (i = 0; i < mas.length; i++) {
+        mas[i].classList.toggle("rate-pick__description_active");
+    }
+}
+
+for (i = 0; i < rateElements.length; i++) {
+    rateElements[i].addEventListener('click', pickRate, false);
+}
+
+// Account Dropdown
+accountSection.addEventListener("click", function (event) {
+    document.getElementById('accountSectionDropdown').classList.toggle("dropdown-toggle");
+});
